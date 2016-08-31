88e93902e7a8e2fbae70d1b05821a27d0f32c916
Jungmo <ajm100@ajou.ac.kr>
2016-08-31 14:03:48 +0900

    Add Accelerometer

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 
 ( function () {
 
-	/*  
-	document.getElementById("btnStart").onclick = function(){
-
-			//sensoron();
-	};
-
-	document.getElementById("btnStop").onclick=
-		function(){
-		sensoroff();
-		};
-	 */
+	var Ax=0;
+	var Ay=0;
+	var Az=0;
+	var HR=0;
+	var worker = new Worker("worker_test.js");
+	
 	window.addEventListener( 'tizenhwkey', function( ev ) {
 		if( ev.keyName === "back" ) {
 			var page = document.getElementsByClassName( 'ui-page-active' )[0],
@@ -27,29 +22,43 @@
 		}
 	} );
 
-
+	
+	// Display sensor value
+	/*
 	var alphaElem = document.getElementById("alpha");
 	var betaElem = document.getElementById("beta");
 	var gammaElem = document.getElementById("gamma");
-
+*/
+	//XXX:Gyro
+	/*
 	window.addEventListener("deviceorientation", function (e) {
 		alphaElem.innerHTML = 'alpha value ' + Math.round(e.alpha);
 		betaElem.innerHTML = 'beta value ' + Math.round(e.beta);
 		gammaElem.innerHTML = 'gamma value ' + Math.round(e.gamma);
 	}, true);
+	*/
+	
+	window.addEventListener('devicemotion', function(e) {
+        Ax = e.accelerationIncludingGravity.x ;
+        Ay = e.accelerationIncludingGravity.y ;
+        Az = e.accelerationIncludingGravity.z ;
+		console.log(Ax, Ay, Az)
+
+	});
 	tizen.humanactivitymonitor.start("HRM",
 			function onSuccess(hrm) {
 		document.getElementById("divHertRate").innerHTML = hrm.heartRate;
+		HR = hrm.heartRate;
 		//console.log("Error "+ hrm.heartRate);
 	});	
-
+	
 
 	document.getElementById("btnStart").onclick = function(){
 		console.log("button clicked");
-		//var dta = '{"metric": "tizen.test","timestamp": 1470381864,"value": 100,"tags": {"acc_z": "z"}}';
-
 	};
-
+	document.getElementById("btnStop").onclick= function(){
+		sensoroff();
+	};
 
 	function test(){
 		var ts = Math.round(new Date().getTime()/1000);
@@ -62,19 +71,7 @@
 		obj2.acc_z = "z";
 		obj.tags = obj2;
 		var dta = JSON.stringify(obj);
-		/*$.post("http://www.w3schools.com/jquery/demo_test_post.asp",
-	          {
-	            metric: "tizen.test",
-	            timestamp: 1470381864,
-	            value: 100,
-	            tags : {
-	  	            acc_z:"z"}
-       },
 
-	          function(data,status){
-	        	var e1 = ('Data: ' + data + '<br> Status: ' + status);
-	        });
-		 */
 		var request = $.ajax({
 			url:"http://202.30.29.209:14242/api/put",
 			type:"POST",
@@ -83,29 +80,7 @@
 			data:dta,
 			cache:false
 		});
-
-
-
 	}
-
-	/*
-	function sensoron(){
-		tizen.humanactivitymonitor.start("HRM",
-		function onSuccess(hrm) {
-			document.getElementById("divHertRate").innerHTML = hrm.heartRate;
-		});		
-
-    	window.addEventListener("devicemotion", function(event) {
-    		document.getElementById("x").innerHTML = event.accelearation.x;
-    		document.getElementById("y").innerHTML = event.accelearation.y;
-    		document.getElementById("z").innerHTML = event.accelearation.z;
-    	}, true)
-
-	}
-	function sensoroff(){
-		tizen.humanactivitymonitor.stop("HRM");
-	}
-	 */
 } () );
 
 
